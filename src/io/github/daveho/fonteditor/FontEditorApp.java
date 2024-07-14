cf1b4fb8b42cca5e5343b92c9d96bccde59d6a31
David Hovemeyer <david.hovemeyer@gmail.com>
2024-07-14 17:10:12 -0400

    added Load and Save buttons

--- src/io/github/daveho/fonteditor/FontEditorApp.java
+++ src/io/github/daveho/fonteditor/FontEditorApp.java
@@ -1,7 +1,12 @@
 package io.github.daveho.fonteditor;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.function.Function;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -15,6 +20,7 @@ public class FontEditorApp extends JPanel implements MyObserver {
 	private BitmapFont model;
 	private BitmapFontView fontView;
 	private GlyphView glyphView;
+	private JPanel buttonPanel;
 	
 	public FontEditorApp() {
 		setLayout(null);
@@ -26,32 +32,62 @@ public class FontEditorApp extends JPanel implements MyObserver {
 
 //		// Create default empty 8x16 bitmap font
 //		model = new BitmapFont(16, 8);
+		
+		this.buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		add(buttonPanel);
+		
+		addButton(buttonPanel, "Load", () -> onLoad());
+		addButton(buttonPanel, "Save", () -> onSave());
 	}
 	
+	private void addButton(JPanel buttonPanel, String name, Runnable fn) {
+		JButton button = new JButton(name);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				fn.run();
+			}
+		});
+		buttonPanel.add(button);
+	}
+
 	public void setModel(BitmapFont model) {
 		this.model = model;
 		
 		fontView.setModel(model);
 		Dimension fontViewDim = fontView.getPreferredSize();
-		fontView.setBounds(BORDER, BORDER, (int) fontViewDim.getWidth(), (int) fontViewDim.getHeight());
 		
 		glyphView.setModel(model);
 		Dimension glyphViewDim = glyphView.getPreferredSize();
+
+		int topPartHeight = (int) (Math.max(fontViewDim.getHeight(), glyphViewDim.getHeight()));
+		int topPartWidth = (int) (BORDER*2 + GAP + fontViewDim.getWidth() + glyphViewDim.getWidth());
+		
+		fontView.setBounds(BORDER, BORDER, (int) fontViewDim.getWidth(), (int) fontViewDim.getHeight());
 		glyphView.setBounds(
 				BORDER + (int) fontViewDim.getWidth() + GAP,
 				BORDER,
 				(int) glyphViewDim.getWidth(),
 				(int) glyphViewDim.getHeight());
 		
-		setPreferredSize(
-				new Dimension(
-						(int) (BORDER*2 + GAP + fontViewDim.getWidth() + glyphViewDim.getWidth()),
-						(int) (BORDER*2 + Math.max(fontViewDim.getHeight(), glyphViewDim.getHeight()))));
+		Dimension buttonPanelDim = buttonPanel.getPreferredSize();
+		buttonPanel.setBounds(BORDER, BORDER + topPartHeight, (int) buttonPanelDim.getWidth(), (int) buttonPanelDim.getHeight());
+
+		setPreferredSize(new Dimension(topPartWidth, topPartHeight + (int) buttonPanelDim.getHeight()));
 	}
 
 	@Override
 	public void update(MyObservable o, Object arg) {
 		// font changed: do nothing for now, but we could keep track of whether
 		// the font has changed since the last save
+	}
+	
+	private void onLoad() {
+		System.out.println("TODO: dialog to load font");
+	}
+	
+	private void onSave() {
+		System.out.println("TODO: dialog to save font");
 	}
 }
